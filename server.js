182955bd082e941f47704f9080d848c63470ec5c
gagan13singh <gagandeepsingh130305@gmail.com>
2026-03-17 20:46:21 +0530

    updates cors

--- server.js
+++ server.js
@@ -35,14 +35,16 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    // allow requests with no origin (like Postman)
     if (!origin) return callback(null, true);
 
-    if (allowedOrigins.includes(origin)) {
+    if (
+      origin.includes("vercel.app") ||
+      origin.includes("localhost")
+    ) {
       return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
     }
+
+    return callback(new Error("Not allowed by CORS"));
   }
 }));
 
